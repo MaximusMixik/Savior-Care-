a146d716269c43845d9b3b913d44da8bfec350d3
MaximusMixik <122988460+MaximusMixik@users.noreply.github.com>
2023-01-18 15:44:14 +0200

    new site

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let header__burger = document.querySelectorAll(".header__burger,.header__link");
+let header__menu = document.querySelector(".header__menu");
+let back = document.querySelector("body");
+
+header__burger.forEach(function (item) {
+  item.onclick = function () {
+    item.classList.toggle("active");
+    header__menu.classList.toggle("active");
+    back.classList.toggle("lock");
+  };
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clicable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button__next",
+    prevEl: ".swiper-button__prev",
+  },
+});
